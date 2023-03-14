2c77ce4a1a6fbddadc9a40fbe207300455a20346
Michael Mayer <39913724+dwaltsch@users.noreply.github.com>
2023-03-14 10:24:13 +0100

    Wetter Abfrage

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,54 +1,42 @@
 //import data from '/secrets/secrets.json';
-import React, {Component} from 'react';
+import React, {Component,useEffect,useState} from 'react';
 import Geocode from "react-geocode";
 
-Geocode.setApiKey("")
 
-const successCallback = (position) => {
-    const {latitude, longitude} = position.coords;
-    Geocode.fromLatLng(latitude, longitude).then(
-        (response) => {
-            const address = response.results[0].formatted_address;
-            let city, state, country;
-            for (let i = 0; i < response.results[0].address_components.length; i++) {
-                for (let j = 0; j < response.results[0].address_components[i].types.length; j++) {
-                    switch (response.results[0].address_components[i].types[j]) {
-                        case "locality":
-                            city = response.results[0].address_components[i].long_name;
-                            break;
-                        case "administrative_area_level_1":
-                            state = response.results[0].address_components[i].long_name;
-                            break;
-                        case "country":
-                            country = response.results[0].address_components[i].long_name;
-                            break;
-                    }
-                }
-            }
-            console.log(city, state, country);
-            console.log(address);
-        },
-        (error) => {
-            console.error(error);
-        }
+function Header() {
+    const [data,setData] = useState([]);
+
+    useEffect(() => {
+        const apikey = ""
+
+        const successCallback = (position) => {
+
+            const {latitude, longitude} = position.coords;
+            // fetch from openweatherapi
+            fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${apikey}&units=metric`)
+                .then(response => response.json())
+                .then(data => {
+                    setData(data);
+                })
+        };
+
+        const errorCallback = (error) => {
+            console.log(error);
+        };
+
+
+        navigator.geolocation.getCurrentPosition(successCallback, errorCallback);
+    }, []);
+
+    return (
+        <div>
+            <h1>Wetter in {data.name}</h1>
+            <h2>{data.weather && data.weather[0].description}</h2>
+            <h2>{data.main && data.main.temp}°C</h2>
+            <h2>{data.main && data.main.humidity}% Luftfeuchtigkeit ?????</h2>
+
+        </div>
     );
-};
-
-const errorCallback = (error) => {
-    console.log(error);
-};
-
-
-navigator.geolocation.getCurrentPosition(successCallback, errorCallback);
-
-class Header extends Component {
-    render() {
-        return (
-            <div>
-                <h1>Weather in {}</h1>
-            </div>
-        );
-    }
 }
 
 export default Header;
