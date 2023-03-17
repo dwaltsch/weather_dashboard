80b10b1e0c989d5c77659dc9d17f56a23860cdc3
silopirt <julian.sonderegger2005@gmail.com>
2023-03-17 08:19:37 +0100

    ...

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import './App.css';
 import Header from "./components/Header";
+import List from './components/List_component/List';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
           <Header></Header>
+          <List></List>
       </header>
     </div>
   );
