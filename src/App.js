841b2889d3624b5d8fff9fc326879b37073f932e
Michael Mayer <39913724+dwaltsch@users.noreply.github.com>
2023-04-18 12:04:42 +0200

    moved around

--- src/App.js
+++ src/App.js
@@ -8,8 +8,8 @@ function App() {
         <div className="App">
             <header className="App-header">
                 <Header></Header>
-                <List></List>
                 <Weatherwidget></Weatherwidget>
+                <List></List>
             </header>
         </div>
     );
