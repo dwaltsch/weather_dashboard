98d8fb08e07230b0f24ee5c827c2d563370e4f3f
Michael Mayer <39913724+dwaltsch@users.noreply.github.com>
2023-04-21 08:31:04 +0200

    Mondphase

--- src/components/Moonphase_component/moonphase.js
+++ src/components/Moonphase_component/moonphase.js
@@ -2,6 +2,40 @@ import React, {useEffect, useState} from 'react';
 import styles from './moonphase.module.css';
 import secret from '../../secret/secret.json';
 
+function moon_phase_recommendations(moon_phase_percent) {
+    if (moon_phase_percent <= 5) {
+        return "Neumond - Konzentriere dich darauf, Absichten zu setzen und neue Projekte zu starten. Vermeide es, große Entscheidungen zu treffen.";
+    } else if (moon_phase_percent <= 10) {
+        return "Zunehmender Mond - Setze deine Ziele in die Tat um. Pflanze Samen und beginne Projekte. Vermeide es, impulsiv zu sein.";
+    } else if (moon_phase_percent <= 15) {
+        return "Erstes Viertel - Stelle dich Herausforderungen. Triff Entscheidungen und handle. Vermeide es, zu konfrontativ zu sein.";
+    } else if (moon_phase_percent <= 20) {
+        return "Zunehmender Halbmond - Verfeinere deine Ziele und Strategien. Konzentriere dich auf Wachstum und Fortschritt. Vermeide es, zu stur zu sein.";
+    } else if (moon_phase_percent <= 25) {
+        return "Vollmond - Feiere deine Erfolge. Zeige Dankbarkeit und genieße den Moment. Vermeide es, zu emotional zu sein.";
+    } else if (moon_phase_percent <= 30) {
+        return "Abnehmender Halbmond - Bewertet deinen Fortschritt und nimm Anpassungen vor. Lass los, was dir nicht mehr dient. Vermeide es, zu selbstkritisch zu sein.";
+    } else if (moon_phase_percent <= 35) {
+        return "Letztes Viertel - Lass los, was dir nicht mehr dient. Reflektiere über gelernte Lektionen. Vermeide es, zu hart mit dir selbst zu sein.";
+    } else if (moon_phase_percent <= 40) {
+        return "Abnehmender Mond - Ruhe dich aus und lade deine Batterien auf. Konzentriere dich auf Selbstpflege und spirituelle Praktiken. Vermeide es, große Entscheidungen zu treffen.";
+    } else if (moon_phase_percent <= 45) {
+        return "Neumond - Konzentriere dich darauf, Absichten zu setzen und neue Projekte zu starten. Vermeide es, große Entscheidungen zu treffen.";
+    } else if (moon_phase_percent <= 50) {
+        return "Zunehmender Mond - Setze deine Ziele in die Tat um. Pflanze Samen und beginne Projekte. Vermeide es, impulsiv zu sein.";
+    } else if (moon_phase_percent <= 55) {
+        return "Erstes Viertel - Stelle dich Herausforderungen. Triff Entscheidungen und handle. Vermeide es, zu konfrontativ zu sein.";
+    } else if (moon_phase_percent <= 60) {
+        return "Zunehmender Halbmond - Verfeinere deine Ziele und Strategien. Konzentriere dich auf Wachstum und Fortschritt. Vermeide es, zu stur zu sein.";
+    } else if (moon_phase_percent <= 65) {
+        return "Vollmond - Feiere deine Erfolge. Zeige Dankbarkeit und genieße den Moment. Vermeide es, zu emotional zu sein.";
+    } else if (moon_phase_percent <= 70) {
+        return "Abnehmender Halbmond - Bewertet deinen Fortschritt und nimm Anpassungen vor. Lass los, was dir nicht mehr dient. Vermeide es, zu selbstkritisch zu sein.";
+    } else if (moon_phase_percent <= 75) {
+        return "Fast Vollmond - Nutze die Energie des bevorstehenden Vollmonds, um deine Pläne zu beschleunigen. Vermeide es, dich zu überfordern und gönne dir genügend Ruhepausen."
+    }
+}
+
 function Moonphase() {
     const [data, setData] = useState([]);
 
@@ -29,8 +63,9 @@ function Moonphase() {
 
     return (
         <div className={styles.container}>
-            <h2>Momentane Mondphase</h2>
-            <p>{((1-data.daily[0].moon_phase)*100).toFixed(2)} %</p>
+            <h2>Mondphase</h2>
+            <p>{((data.daily[0].moon_phase)*100).toFixed(0)} % beleuchtet</p>
+            <p>{moon_phase_recommendations((data.daily[0].moon_phase)*100)}</p>
         </div>
     );
 }
