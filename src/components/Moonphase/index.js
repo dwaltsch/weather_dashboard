8e3f9fa82656a10d5efa54b072f8030e620b0046
Michael Mayer <39913724+dwaltsch@users.noreply.github.com>
2023-04-28 09:26:02 +0200

    mondphase

--- src/components/Moonphase/index.js
+++ src/components/Moonphase/index.js
@@ -4,35 +4,29 @@ import secret from '../../secret/secret.json';
 
 function moon_phase_recommendations(moon_phase_percent) {
     if (moon_phase_percent <= 5) {
-        return "Neumond - Konzentriere dich darauf, Absichten zu setzen und neue Projekte zu starten. Vermeide es, große Entscheidungen zu treffen.";
-    } else if (moon_phase_percent <= 10) {
-        return "Zunehmender Mond - Setze deine Ziele in die Tat um. Pflanze Samen und beginne Projekte. Vermeide es, impulsiv zu sein.";
-    } else if (moon_phase_percent <= 15) {
-        return "Erstes Viertel - Stelle dich Herausforderungen. Triff Entscheidungen und handle. Vermeide es, zu konfrontativ zu sein.";
+        return "Neumond - Setze Absichten, starte neue Projekte, vermeide große Entscheidungen.";
     } else if (moon_phase_percent <= 20) {
-        return "Zunehmender Halbmond - Verfeinere deine Ziele und Strategien. Konzentriere dich auf Wachstum und Fortschritt. Vermeide es, zu stur zu sein.";
-    } else if (moon_phase_percent <= 25) {
-        return "Vollmond - Feiere deine Erfolge. Zeige Dankbarkeit und genieße den Moment. Vermeide es, zu emotional zu sein.";
-    } else if (moon_phase_percent <= 30) {
-        return "Abnehmender Halbmond - Bewertet deinen Fortschritt und nimm Anpassungen vor. Lass los, was dir nicht mehr dient. Vermeide es, zu selbstkritisch zu sein.";
-    } else if (moon_phase_percent <= 35) {
-        return "Letztes Viertel - Lass los, was dir nicht mehr dient. Reflektiere über gelernte Lektionen. Vermeide es, zu hart mit dir selbst zu sein.";
-    } else if (moon_phase_percent <= 40) {
-        return "Abnehmender Mond - Ruhe dich aus und lade deine Batterien auf. Konzentriere dich auf Selbstpflege und spirituelle Praktiken. Vermeide es, große Entscheidungen zu treffen.";
-    } else if (moon_phase_percent <= 45) {
-        return "Neumond - Konzentriere dich darauf, Absichten zu setzen und neue Projekte zu starten. Vermeide es, große Entscheidungen zu treffen.";
-    } else if (moon_phase_percent <= 50) {
-        return "Zunehmender Mond - Setze deine Ziele in die Tat um. Pflanze Samen und beginne Projekte. Vermeide es, impulsiv zu sein.";
-    } else if (moon_phase_percent <= 55) {
-        return "Erstes Viertel - Stelle dich Herausforderungen. Triff Entscheidungen und handle. Vermeide es, zu konfrontativ zu sein.";
-    } else if (moon_phase_percent <= 60) {
-        return "Zunehmender Halbmond - Verfeinere deine Ziele und Strategien. Konzentriere dich auf Wachstum und Fortschritt. Vermeide es, zu stur zu sein.";
+        if (moon_phase_percent <= 10) {
+            return "Zunehmender Mond - Setze Ziele um, beginne Projekte, vermeide Impulsivität.";
+        } else if (moon_phase_percent <= 15) {
+            return "Erstes Viertel - Stelle dich Herausforderungen, triff Entscheidungen, handle, vermeide Konfrontation.";
+        } else {
+            return "Zunehmender Halbmond - Verfeinere Ziele und Strategien, konzentriere dich auf Wachstum und Fortschritt, vermeide Sturheit.";
+        }
     } else if (moon_phase_percent <= 65) {
-        return "Vollmond - Feiere deine Erfolge. Zeige Dankbarkeit und genieße den Moment. Vermeide es, zu emotional zu sein.";
-    } else if (moon_phase_percent <= 70) {
-        return "Abnehmender Halbmond - Bewertet deinen Fortschritt und nimm Anpassungen vor. Lass los, was dir nicht mehr dient. Vermeide es, zu selbstkritisch zu sein.";
-    } else if (moon_phase_percent <= 75) {
-        return "Fast Vollmond - Nutze die Energie des bevorstehenden Vollmonds, um deine Pläne zu beschleunigen. Vermeide es, dich zu überfordern und gönne dir genügend Ruhepausen."
+        if (moon_phase_percent <= 25) {
+            return "Vollmond - Feiere Erfolge, zeige Dankbarkeit, genieße den Moment, vermeide Überemotion.";
+        } else if (moon_phase_percent <= 35) {
+            return "Abnehmender Halbmond - Bewertet Fortschritt, nimm Anpassungen vor, lass los, vermeide Selbstkritik.";
+        } else {
+            return "Fast Vollmond - Nutze die Energie des bevorstehenden Vollmonds, um deine Pläne zu beschleunigen. Vermeide Überforderung, gönne dir genügend Ruhepausen.";
+        }
+    } else {
+        if (moon_phase_percent <= 80) {
+            return "Letztes Viertel - Lass los, reflektiere gelernte Lektionen, vermeide Selbstkritik.";
+        } else {
+            return "Abnehmender Mond - Ruhe dich aus, lade Batterien auf, konzentriere dich auf Selbstpflege und Spiritualität, vermeide große Entscheidungen.";
+        }
     }
 }
 
@@ -63,12 +57,12 @@ function Moonphase() {
 
     return (
         <div className={styles.container}>
-            <h2>Mondphase</h2>
-            <img class="moonimage" style={{height: '75px'}}
+            <img className="moonimage" style={{height: '65px', width: '65px', margin: 'auto', display: 'flex', justifyContent: 'left', padding: '18px'}}
                  src={`https://www.timeanddate.de/scripts/moon.php?i=${data.daily[0].moon_phase}&p=10&r=0.883`}
                  alt="weather icon"/>
+            <h2>Mondphase</h2>
             <h3>{((data.daily[0].moon_phase) * 100).toFixed(0)} % beleuchtet</h3>
-            <p>{moon_phase_recommendations((data.daily[0].moon_phase) * 100)}</p>
+            <p style={{fontSize: '15px', textAlign: 'center'}}>{moon_phase_recommendations((data.daily[0].moon_phase) * 100)}</p>
         </div>
     );
 }
--- src/components/Weatherwidget/index.js
+++ src/components/Weatherwidget/index.js
@@ -36,13 +36,17 @@ function WeatherwidgetComponent() {
         <div className={styles.container}>
             <img src={`http://openweathermap.org/img/wn/${data.current.weather[0].icon}@4x.png`} alt="weather icon"/>
             <h1>{parseInt(data.current.temp.toFixed(1))}°C</h1>
-            <h2>Luftfeuchtigkeit: {data.current.humidity} %</h2>
             <h2>{data.current.weather[0].description}</h2>
+            <h2>Luftfeuchtigkeit: {data.current.humidity} %</h2>
             <h2>Wind: {Math.round(parseFloat(data.current.wind_speed))} m/s Richtung {getWindDirection(data.current.wind_deg)}</h2>
             <h2>
-                Regenwahrscheinlichkeit{' '}
-                {typeof data.hourly[0].rain !== 'undefined' ? data.hourly[0].rain['1h'] * 100 : 0} %
+                Wahrscheinlichkeit{' '}
+                {data.hourly[0].pop !== 0 ? Math.round(data.hourly[0].pop * 100) : 0} %
             </h2>
+                <h2>
+                    Menge{' '}
+                    {typeof data.hourly[0].rain !== 'undefined' ? data.hourly[0].rain['1h'] : 0} mm
+                </h2>
         </div>
     );
 }
