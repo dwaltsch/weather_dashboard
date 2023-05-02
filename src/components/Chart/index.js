b3dd21421b49c65f70a136742a7e5dc650ee8ae1
Julian Sonderegger <julian.sonderegger2005@gmail.com>
2023-05-02 15:04:49 +0200

    endlich alle merge conflicte weg

--- src/components/Chart/index.js
+++ src/components/Chart/index.js
@@ -1,65 +1,89 @@
-import React, {useEffect, useState} from "react";
+import React, { useEffect, useState } from "react";
 
 import secret from "../../secret/secret.json";
 
-import {Line} from "react-chartjs-2";
-import {CategoryScale, Chart as ChartJS, Legend, LinearScale, LineElement, PointElement,} from "chart.js";
+import { Line } from "react-chartjs-2";
+import {
+  CategoryScale,
+  Chart as ChartJS,
+  Legend,
+  LinearScale,
+  LineElement,
+  PointElement,
+} from "chart.js";
 
 ChartJS.register(LineElement, CategoryScale, LinearScale, PointElement, Legend);
 
 function Index() {
-    const [data, setData] = useState([]);
+  const [data, setData] = useState([]);
 
-    useEffect(() => {
-        const successCallback = (position) => {
-            const {latitude, longitude} = position.coords;
-            // fetch from openweatherapi
-            fetch(
-                `https://api.openweathermap.org/data/2.5/forecast?lat=${latitude}&lon=${longitude}&appid=${secret.apiKey}&units=metric&cnt=5`
-            )
-                .then((response) => response.json())
-                .then(({list}) => {
-                    setData(list);
-                });
-        };
+  useEffect(() => {
+    const successCallback = (position) => {
+      const { latitude, longitude } = position.coords;
+      // fetch from openweatherapi
+      fetch(
+        `https://api.openweathermap.org/data/2.5/forecast?lat=${latitude}&lon=${longitude}&appid=${secret.apiKey}&units=metric&cnt=5`
+      )
+        .then((response) => response.json())
+        .then(({ list }) => {
+          setData(list);
+        });
+    };
 
-        const errorCallback = (error) => {
-            console.error(error);
+    const errorCallback = (error) => {
+      console.error(error);
+    };
 
-        };
+    navigator.geolocation.getCurrentPosition(successCallback, errorCallback);
+  }, []);
+  if (!data) {
+    return <p>Loading...</p>;
+  }
+  console.log(data);
 
-        navigator.geolocation.getCurrentPosition(successCallback, errorCallback);
-    }, []);
-    if (!data) {
-        return <p>Loading...</p>;
-    }
-    console.log(data)
-
-    let dateArray = [];
-    let temperatureArray = [];
-    let temperatureFeelsLike = [];
-    data.map(({dt, main}) => {
-        dateArray.push(new Date(dt * 1000).toLocaleTimeString("de-DE"));
-        temperatureArray.push(main.temp)
-        temperatureFeelsLike.push(main.feels_like)
-    })
+  let dateArray = [];
+  let temperatureArray = [];
+  let temperatureFeelsLike = [];
+  data.map(({ dt, main }) => {
+    dateArray.push(new Date(dt * 1000).toLocaleTimeString("de-DE"));
+    temperatureArray.push(main.temp);
+    temperatureFeelsLike.push(main.feels_like);
+  });
   const dataForChart = {
-    labels: [dateArray[0],dateArray[1],dateArray[2],dateArray[3],dateArray[4]],
+    labels: [
+      dateArray[0],
+      dateArray[1],
+      dateArray[2],
+      dateArray[3],
+      dateArray[4],
+    ],
     datasets: [
       {
-        data: [temperatureArray[0],temperatureArray[1],temperatureArray[2],temperatureArray[3],temperatureArray[4]],
+        data: [
+          temperatureArray[0],
+          temperatureArray[1],
+          temperatureArray[2],
+          temperatureArray[3],
+          temperatureArray[4],
+        ],
         backgroundColor: "transparent",
         borderColor: "#555555",
         pointBorderColor: "transparent",
         pointBorderWidth: 4,
       },
       {
-        data: [temperatureFeelsLike[0],temperatureFeelsLike[1],temperatureFeelsLike[2],temperatureFeelsLike[3],temperatureFeelsLike[4]],
-        backgroundColor: "white",
+        data: [
+          temperatureFeelsLike[0],
+          temperatureFeelsLike[1],
+          temperatureFeelsLike[2],
+          temperatureFeelsLike[3],
+          temperatureFeelsLike[4],
+        ],
+        backgroundColor: "transparent",
         borderColor: "white",
-        pointBorderColor: "white",
-        pointBorderWidth: 4,  
-      }
+        pointBorderColor: "transparent",
+        pointBorderWidth: 4,
+      },
     ],
   };
   const options = {
@@ -70,24 +94,23 @@ function Index() {
       x: {
         grid: {
           display: false,
-          color: 'rgba(0,0,0,0.5)',
-          lineWidth:1
+          color: "rgba(0,0,0,0.5)",
+          lineWidth: 1,
         },
-        ticks:{
-          color:"#000000"
-        }
+        ticks: {
+          color: "#000000",
+        },
       },
-      y:{
+      y: {
         grid: {
-          color: 'rgba(0,0,0,0.5)',
-          lineWidth:1  // <-- this line is answer to initial question
+          color: "rgba(0,0,0,0.5)",
+          lineWidth: 1, // <-- this line is answer to initial question
         },
-        ticks:{
-            stepSize: 2,
-            color: "#000000"
-            
-        }
-      }
+        ticks: {
+          stepSize: 2,
+          color: "#000000",
+        },
+      },
     },
   };
   return <Line data={dataForChart} options={options}></Line>;
